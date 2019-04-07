df4e8b1276c92bab82e8e66ee4bf5570963acff1
Felipe B. Netto <felipe.netto@mobiauto.com.br>
2019-04-07 12:25:31 -0300

    included file loader and sass preprocessors, and changed file structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
+import 'nes.css/css/nes.min.css';
+import '../styles/scss/styles.scss';
+
 let workingBtn = document.getElementById('working-btn');
 
 workingBtn.addEventListener('click', () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
 const path = require("path");
-var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/js/index.js',
+    entry: './src/js/app.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'src/js/dist')
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(s*)css$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'images/'
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
         ]
     }
 }
