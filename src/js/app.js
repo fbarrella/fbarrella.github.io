bbd620dbbc055691b70bfcc30f5e6640f8fbfa40
Felipe B. Netto <felipe.netto@mobiauto.com.br>
2019-04-15 22:17:50 -0300

    wip: changes to site style

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,2 @@
 import 'nes.css/css/nes.min.css';
 import '../styles/scss/styles.scss';
-
-let workingBtn = document.getElementById('working-btn');
-
-workingBtn.addEventListener('click', () => {
-    alert('Hello World!');
-})
